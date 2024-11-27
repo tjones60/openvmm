806ecd2961bf128c4574b446fe6efed9d76cb991
Trevor Jones <jonestrevor@microsoft.com>
2024-11-27 15:48:14 -0800

    powershell

--- petri/src/vm/hyperv/construct.rs
+++ petri/src/vm/hyperv/construct.rs
@@ -7,8 +7,8 @@ use anyhow::Context as _;
 
 /// Runs New-VM with the given arguments.
 pub fn run_new_vm(vm: &str) -> anyhow::Result<()> {
-    let mut cmd = std::process::Command::new("New-VM");
-    cmd.arg("-Name").arg(vm);
+    let mut cmd = std::process::Command::new("powershell.exe");
+    cmd.arg("New-VM").arg("-Name").arg(vm);
     let status = cmd.status().context("failed run New-VM")?;
     if !status.success() {
         anyhow::bail!("New-VM failed with exit code: {}", status);
@@ -18,8 +18,8 @@ pub fn run_new_vm(vm: &str) -> anyhow::Result<()> {
 
 /// Runs New-VM with the given arguments.
 pub fn run_remove_vm(vm: &str) -> anyhow::Result<()> {
-    let mut cmd = std::process::Command::new("Remove-VM");
-    cmd.arg("-Name").arg(vm);
+    let mut cmd = std::process::Command::new("powershell.exe");
+    cmd.arg("Remove-VM").arg("-Name").arg(vm);
     let status = cmd.status().context("failed run Remove-VM")?;
     if !status.success() {
         anyhow::bail!("Remove-VM failed with exit code: {}", status);
