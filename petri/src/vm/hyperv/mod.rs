498665ed3bc5ced547d0bdb5d145b0c80a222fd0
Trevor Jones <jonestrevor@microsoft.com>
2024-12-11 15:10:31 -0800

    start and stop manually

--- petri/src/vm/hyperv/mod.rs
+++ petri/src/vm/hyperv/mod.rs
@@ -7,31 +7,30 @@ mod construct;
 mod modify;
 mod runtime;
 
-pub struct PetriVmConfigHyperV {
-    name: String,
-}
+pub use construct::HyperVConfig;
+pub use construct::HyperVGeneration;
+
+pub struct PetriVmConfigHyperV(HyperVConfig);
 
 pub struct PetriVmHyperV {
-    name: String,
+    config: HyperVConfig,
 }
 
 impl PetriVmConfigHyperV {
     /// Build and boot the requested VM
     pub fn run(self) -> anyhow::Result<PetriVmHyperV> {
-        let PetriVmConfigHyperV { name } = self;
+        construct::run_new_vm(&self.0)?;
+        runtime::hvc_start(&self.0.name)?;
 
-        construct::run_new_vm(&name)?;
-        runtime::hvc_start(&name)?;
-
-        Ok(PetriVmHyperV { name })
+        Ok(PetriVmHyperV { config: self.0 })
     }
 }
 
 impl PetriVmHyperV {
     /// Wait for VM to stop
     pub fn wait_for_teardown(self) -> anyhow::Result<HaltReason> {
-        runtime::hvc_wait_for_power_off(&self.name)?;
-        construct::run_remove_vm(&self.name)?;
+        runtime::hvc_wait_for_power_off(&self.config.name)?;
+        construct::run_remove_vm(&self.config.name)?;
 
         Ok(HaltReason::PowerOff)
     }
@@ -39,13 +38,20 @@ impl PetriVmHyperV {
 
 #[cfg(test)]
 mod test {
+    use super::HyperVConfig;
+    use super::HyperVGeneration;
     use super::PetriVmConfigHyperV;
+    use std::path::PathBuf;
 
     #[test]
     fn hyperv_test() {
-        let config = PetriVmConfigHyperV {
+        let config = PetriVmConfigHyperV(HyperVConfig {
             name: "petritestvm".to_string(),
-        };
+            generation: Some(HyperVGeneration::Two),
+            memory_startup_bytes: Some(0x1_0000_0000),
+            path: None,
+            vhd_path: Some(PathBuf::from("E:\\cross\\disk.vhdx")),
+        });
         let vm = config.run().unwrap();
         vm.wait_for_teardown().unwrap();
     }
