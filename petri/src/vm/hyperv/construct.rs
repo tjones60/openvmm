3614250606568fd93df59a9568438f3f072e7d6f
Trevor Jones <jonestrevor@microsoft.com>
2024-12-11 15:10:31 -0800

    start and stop manually

--- petri/src/vm/hyperv/construct.rs
+++ petri/src/vm/hyperv/construct.rs
@@ -4,11 +4,62 @@
 //! Functions for creating Hyper-V VMs.
 
 use anyhow::Context as _;
+use std::fmt::Display;
+use std::path::PathBuf;
+use std::process::Command;
+
+pub enum HyperVGeneration {
+    One,
+    Two,
+}
+
+impl Display for HyperVGeneration {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            HyperVGeneration::One => {
+                write!(f, "1")
+            }
+            HyperVGeneration::Two => {
+                write!(f, "2")
+            }
+        }
+    }
+}
+
+pub struct HyperVConfig {
+    /// Specifies the name of the new virtual machine.
+    pub name: String,
+    // /// Specifies the device to use as the boot device for the new virtual machine.
+    // pub boot_device: HyperVBootDevice,
+    /// Specifies the generation for the virtual machine.
+    pub generation: Option<HyperVGeneration>,
+    /// Specifies the amount of memory, in bytes, to assign to the virtual machine.
+    pub memory_startup_bytes: Option<u64>,
+    /// Specifies the directory to store the files for the new virtual machine.
+    pub path: Option<PathBuf>,
+    /// Specifies the path to a virtual hard disk file.
+    pub vhd_path: Option<PathBuf>,
+}
 
 /// Runs New-VM with the given arguments.
-pub fn run_new_vm(vm: &str) -> anyhow::Result<()> {
-    let mut cmd = std::process::Command::new("powershell.exe");
-    cmd.arg("New-VM").arg("-Name").arg(vm);
+pub fn run_new_vm(config: &HyperVConfig) -> anyhow::Result<()> {
+    let mut cmd = Command::new("powershell.exe");
+    cmd.arg("New-VM").arg("-Force");
+    cmd.arg("-Name").arg(&config.name);
+    if let Some(generation) = &config.generation {
+        cmd.arg("-Generation").arg(generation.to_string());
+    }
+    if let Some(memory_startup_bytes) = config.memory_startup_bytes {
+        cmd.arg("-MemoryStartupBytes")
+            .arg(memory_startup_bytes.to_string());
+    }
+    if let Some(path) = &config.path {
+        cmd.arg("-Path").arg(path);
+    }
+    if let Some(vhd_path) = &config.vhd_path {
+        cmd.arg("-VHDPath").arg(vhd_path);
+    }
+
     let status = cmd.status().context("failed run New-VM")?;
     if !status.success() {
         anyhow::bail!("New-VM failed with exit code: {}", status);
@@ -18,8 +69,8 @@ pub fn run_new_vm(vm: &str) -> anyhow::Result<()> {
 
 /// Runs New-VM with the given arguments.
 pub fn run_remove_vm(vm: &str) -> anyhow::Result<()> {
-    let mut cmd = std::process::Command::new("powershell.exe");
-    cmd.arg("Remove-VM").arg("-Name").arg(vm);
+    let mut cmd = Command::new("powershell.exe");
+    cmd.arg("Remove-VM").arg("-Name").arg(vm).arg("-Force");
     let status = cmd.status().context("failed run Remove-VM")?;
     if !status.success() {
         anyhow::bail!("Remove-VM failed with exit code: {}", status);
--- petri/src/vm/hyperv/runtime.rs
+++ petri/src/vm/hyperv/runtime.rs
@@ -14,8 +14,53 @@ pub fn hvc_start(vm: &str) -> anyhow::Result<()> {
 //     run_hvc(|cmd| cmd.arg("kill").arg(vm))
 // }
 
+/// HyperV VM state as reported by hvc
+pub enum VmState {
+    /// The VM is powered off.
+    Off,
+    /// The VM is powered on.
+    On,
+    /// The VM is powering on.
+    Starting,
+    /// The VM is powering off.
+    Stopping,
+    /// The VM has been saved.
+    Saved,
+    /// The VM has been paused.
+    Paused,
+    /// The VM is being reset.
+    Resetting,
+    /// The VM is saving.
+    Saving,
+    /// The VM is pausing.
+    Pausing,
+    /// The VM is resuming.
+    Resuming,
+    /// Error getting the VM state.
+    Unknown,
+}
+
+pub fn hvc_state(vm: &str) -> VmState {
+    hvc_output(|cmd| cmd.arg("state").arg(vm)).map_or_else(
+        |_| VmState::Unknown,
+        |s| match s.trim_end() {
+            "off" => VmState::Off,
+            "on" => VmState::On,
+            "starting" => VmState::Starting,
+            "stopping" => VmState::Stopping,
+            "saved" => VmState::Saved,
+            "paused" => VmState::Paused,
+            "resetting" => VmState::Resetting,
+            "saving" => VmState::Saving,
+            "pausing" => VmState::Pausing,
+            "resuming" => VmState::Resuming,
+            _ => VmState::Unknown,
+        },
+    )
+}
+
 pub fn hvc_wait_for_power_off(vm: &str) -> anyhow::Result<()> {
-    while hvc_output(|cmd| cmd.arg("state").arg(vm))? != "off" {
+    while !matches!(hvc_state(vm), VmState::Off) {
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 
