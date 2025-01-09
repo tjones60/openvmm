7e99a79c8186c9c851c702b752d5ecfcefd9f8b6
Ben Hillis <benhillis@gmail.com>
2025-01-09 09:33:50 -0800

    Flowey: Use explicit parameter names instead of auto generated. (#636)
    This change moves from auto-generated parameter names to requiring the
    caller to specify a variable name. This makes for easier to read
    generated pipeline files and allows for better behavior if another
    pipeline wants to use queue-time variables to control pipeline behavior.
    
    ---------
    
    Co-authored-by: Ben Hillis <benhill@ntdev.microsoft.com>

--- flowey/flowey_cli/src/pipeline_resolver/github_yaml/mod.rs
+++ flowey/flowey_cli/src/pipeline_resolver/github_yaml/mod.rs
@@ -364,10 +364,12 @@ echo "{RUNNER_TEMP}/work" | {var_db_insert_working_dir}
 
             let var_db_inject_cmd = bootstrap_bash_var_db_inject(flowey_var, is_string);
 
+            let name = parameters[*pipeline_param_idx].name();
+
             let cmd = format!(
                 r#"
 cat <<'EOF' | {var_db_inject_cmd}
-${{{{ inputs.param{pipeline_param_idx} != '' && inputs.param{pipeline_param_idx} || '{default}' }}}}
+${{{{ inputs.{name} != '' && inputs.{name} || '{default}' }}}}
 EOF
 "#
             )
@@ -584,13 +586,14 @@ EOF
             inputs: github_yaml_defs::Inputs {
                 inputs: parameters
                     .into_iter()
-                    .enumerate()
-                    .map(|(idx, param)| {
+                    .map(|param| {
                         (
-                            format!("param{idx}"),
+                            param.name().to_string(),
                             match param {
                                 flowey_core::pipeline::internal::Parameter::Bool {
+                                    name: _,
                                     description,
+                                    kind: _,
                                     default,
                                 } => github_yaml_defs::Input {
                                     description: Some(description.clone()),
@@ -599,7 +602,9 @@ EOF
                                     ty: github_yaml_defs::InputType::Boolean,
                                 },
                                 flowey_core::pipeline::internal::Parameter::String {
+                                    name: _,
                                     description,
+                                    kind: _,
                                     default,
                                     possible_values: _,
                                 } => github_yaml_defs::Input {
@@ -611,7 +616,9 @@ EOF
                                     ty: github_yaml_defs::InputType::String,
                                 },
                                 flowey_core::pipeline::internal::Parameter::Num {
+                                    name: _,
                                     description,
+                                    kind: _,
                                     default,
                                     possible_values: _,
                                 } => github_yaml_defs::Input {
