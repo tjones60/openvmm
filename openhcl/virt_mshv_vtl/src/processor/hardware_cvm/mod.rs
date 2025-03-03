381a882c91ceefc7340e79ace2470c2b8c310e3b
Steven Malis <137308034+smalis-msft@users.noreply.github.com>
2025-03-03 07:41:41 -0800

    TDX: Fill in get_vsm_capabilities and properly mark DR6 as shared (#915)
    This is a partial cherry-pick of
    https://github.com/microsoft/openvmm/commit/6e390e91bc33f490f24067c3377544b2feef0df0

--- openhcl/virt_mshv_vtl/src/processor/hardware_cvm/mod.rs
+++ openhcl/virt_mshv_vtl/src/processor/hardware_cvm/mod.rs
@@ -305,7 +305,9 @@ impl<T, B: HardwareIsolatedBacking> UhHypercallHandler<'_, '_, T, B> {
             )
             .into()),
             HvX64RegisterName::VsmCapabilities => Ok(u64::from(
-                hvdef::HvRegisterVsmCapabilities::new().with_deny_lower_vtl_startup(true),
+                hvdef::HvRegisterVsmCapabilities::new()
+                    .with_deny_lower_vtl_startup(true)
+                    .with_dr6_shared(self.vp.partition.hcl.dr6_shared()),
             )
             .into()),
             HvX64RegisterName::VpAssistPage => Ok(self.vp.backing.cvm_state_mut().hv[vtl]
