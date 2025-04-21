e370cb21f4ffc9e2ec7a145a16bb293d5c46a4f3
Trevor Jones <jonestrevor@microsoft.com>
2025-04-21 16:53:09 -0700

    set computer name

--- petri/make_imc_hive/src/windows/mod.rs
+++ petri/make_imc_hive/src/windows/mod.rs
@@ -9,27 +9,49 @@ use anyhow::Context;
 pub(crate) fn main() -> anyhow::Result<()> {
     let path = std::env::args_os().nth(1).context("missing path")?;
     let hive = Hive::create()?;
-    {
-        let mut key;
-        let mut parent = hive.as_ref();
-        for subkey in ["SYSTEM", "CurrentControlSet", "Services", "pipette"] {
-            let new_key = parent.create_key(subkey)?;
-            key = new_key;
-            parent = key.as_ref();
-        }
 
-        parent.set_dword("Type", 0x10)?; // win32 service
-        parent.set_dword("Start", 2)?; // auto start
-        parent.set_dword("ErrorControl", 1)?; // normal
-        parent.set_sz("ImagePath", "D:\\pipette.exe --service")?;
-        parent.set_sz("DisplayName", "Petri pipette agent")?;
-        parent.set_sz("ObjectName", "LocalSystem")?;
-        parent.set_multi_sz("DependOnService", ["RpcSs"])?;
+    let key_system = hive.create_key("SYSTEM")?;
+    {
+        let key_current_control_set = key_system.create_key("CurrentControlSet")?;
+        {
+            let key_control = key_current_control_set.create_key("Control")?;
+            {
+                let key_computer_name = key_control.create_key("ComputerName")?;
+                {
+                    let key_computer_name_inner = key_computer_name.create_key("ComputerName")?;
+
+                    key_computer_name_inner.set_sz("ComputerName", "ImcVM")?;
+                }
+            }
+        }
+        {
+            let key_services = key_current_control_set.create_key("Services")?;
+            {
+                let key_tcpip = key_services.create_key("Tcpip")?;
+                {
+                    let key_parameters = key_tcpip.create_key("Parameters")?;
+
+                    key_parameters.set_sz("Hostname", "ImcVM")?;
+                    key_parameters.set_sz("NV Hostname", "ImcVM")?;
+                }
+            }
+            {
+                let key_pipette = key_services.create_key("pipette")?;
+
+                key_pipette.set_dword("Type", 0x10)?; // win32 service
+                key_pipette.set_dword("Start", 2)?; // auto start
+                key_pipette.set_dword("ErrorControl", 1)?; // normal
+                key_pipette.set_sz("ImagePath", "D:\\pipette.exe --service")?;
+                key_pipette.set_sz("DisplayName", "Petri pipette agent")?;
+                key_pipette.set_sz("ObjectName", "LocalSystem")?;
+                key_pipette.set_multi_sz("DependOnService", ["RpcSs"])?;
+            }
+        }
     }
 
     // Windows defaults to 1, so we need to set it to 2 to cause Windows to
     // apply the IMC changes on first boot.
-    hive.set_dword("Sequence", 2)?;
+    hive.set_dword("Sequence", 0xf)?;
 
     let _ = std::fs::remove_file(&path);
     hive.save(path.as_ref())?;
