b60895f59c7e4482926634ed8c086852b44d6634
Trevor Jones <jonestrevor@microsoft.com>
2025-06-19 13:46:48 -0700

    remove one suppression that is too general

--- vm/devices/firmware/firmware_uefi/src/service/diagnostics.rs
+++ vm/devices/firmware/firmware_uefi/src/service/diagnostics.rs
@@ -48,11 +48,10 @@ pub const MAX_MESSAGE_LENGTH: u16 = 0x1000; // 4KB
 // These messages are the result of known issues with our UEFI firmware that do
 // not seem to affect the guest.
 // TODO: Fix UEFI to resolve this errors/warnings
-const SUPPRESS_LOGS: [&str; 4] = [
+const SUPPRESS_LOGS: [&str; 3] = [
     "WARNING: There is mismatch of supported HashMask (0x2 - 0x7) between modules",
     "that are linking different HashInstanceLib instances!",
     "ConvertPages: failed to find range",
-    "Dxe.efi",
 ];
 
 /// Represents a processed log entry from the EFI diagnostics buffer
