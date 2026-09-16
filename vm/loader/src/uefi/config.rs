3340c99b7835cb78c64c03d3b5d6aaf7b7de6a5e
Maheer Aeron <maheeraeron@microsoft.com>
2026-09-16 08:27:54 -0700

    config: Rename hv_sint_enabled in uefi config flags to disable_sha1_pcr (#4457)
    This follows a mu_msvm PR that introduces a config flag that masks sha1
    in our TPM PCRs:
    https://github.com/microsoft/mu_msvm/commit/8290e081b910b8965ead5cb4792a7dc3a17d8c3d.
    
    It takes over the old hv_sint_enabled bit in the UEFI_CONFIG_FLAGS
    struct, so this PR simply renames the bit.
    
    We intentionally are leaving this value to be false always, until future
    changes come in to enable this for TPM 1.85

--- vm/loader/src/uefi/config.rs
+++ vm/loader/src/uefi/config.rs
@@ -332,7 +332,7 @@ pub struct Flags {
     pub dhcp6_link_layer_address: bool,
     pub cxl_memory_enabled: bool,
     pub mtrrs_initialized_at_load: bool,
-    _reserved_hv_sint: bool,
+    pub disable_sha1_pcr: bool,
     pub vmbus_disabled: bool,
     pub pci_resources_pre_assigned: bool,
     pub force_dma_bounce_enabled: bool,
