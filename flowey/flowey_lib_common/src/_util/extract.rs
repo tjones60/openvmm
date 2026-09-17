b5387e8688e8b9ee36098cea5da36f3349f992f2
Trevor Jones <jonestrevor@microsoft.com>
2026-09-16 17:22:07 -0700

    all types

--- flowey/flowey_lib_common/src/_util/extract.rs
+++ flowey/flowey_lib_common/src/_util/extract.rs
@@ -168,6 +168,10 @@ pub fn extract_tar_gz_if_new(
         flowey::shell_cmd!(rt, "tar -xf {file}").run()?;
 
         fs_err::write(pkg_info_file, file_version)?;
+
+        // change back to the root dir so subsequent extractions don't get
+        // nested when there is no persistent dir
+        rt.sh.change_dir(&root_dir);
     } else {
         log::info!("already extracted!");
     }
@@ -261,6 +265,10 @@ pub fn extract_tar_bz2_if_new(
         flowey::shell_cmd!(rt, "tar -xf {file}").run()?;
 
         fs_err::write(pkg_info_file, file_version)?;
+
+        // change back to the root dir so subsequent extractions don't get
+        // nested when there is no persistent dir
+        rt.sh.change_dir(&root_dir);
     } else {
         log::info!("already extracted!");
     }
